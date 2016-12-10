1f2a112afb19921a1aae9f3e9aa8cbe28e68f8c6
Eric Hall <halleri@onid.oregonstate.edu>
2016-12-10 15:59:28 -0800

    add weather api

---
+++ Javascript and Node/Weather API/weather.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var browser = require('browserify');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home') 
+});
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+
+//app.listen(app.get('port'), function(){
+ // console.log('Express started on :' + app.get('port') + '; press Ctrl-C to terminate.');
+//});
